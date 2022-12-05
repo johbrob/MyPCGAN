2b7c3cc21a3f7b2e004451edb01f1b62d877bb85
johbrob <johan.broberg92@gmail.com>
2022-12-05 09:40:46 +0100

    updated dataset class

--- DataManaging/AudioDatasets.py
+++ DataManaging/AudioDatasets.py
@@ -11,19 +11,22 @@ import os
 from DatasetCreation import create_audio_dataset
 
 
-def save_sample(file, sampling_rate, audio):
-    audio = (audio.numpy() * 32768).astype("int16")
-    scipy.io.wavfile.write(file, sampling_rate, audio)
+def load_wav_to_torch(path, sampling_rate):
+    data, sampling_rate = librosa.core.load(path, sr=sampling_rate)
+    data = 0.95 * librosa.util.normalize(data)
+    return torch.from_numpy(data).float(), sampling_rate
+
 
 
 class AudioDataset(Dataset):
 
-    def __init__(self, annotations):
-        # self.sampling_rate = sampling_rate
+    def __init__(self, annotations, sampling_rate):
+        self.sampling_rate = sampling_rate
         # self.segment_length = segment_length
 
-        self.audio_files = annotations['file'].to_numpy()
+        self.audio_files = annotations['preprocessed_file'].to_numpy()
         self.gender_idx = annotations['gender'].to_numpy()
+        self.labels = annotations['label'].to_numpy()
         self.speaker_id = annotations['speaker_id'].to_numpy()
 
         # self._preprocess()
@@ -36,23 +39,27 @@ class AudioDataset(Dataset):
         # data, sr = self._load_preprocessed(data_path)
 
 
-    def _load_preprocessed(self, path):
+    def _load_preprocessed_file(self, path):
         data, sampling_rate = librosa.core.load(path, sr=self.sampling_rate)
         data = 0.95 * librosa.util.normalize(data)
         return torch.from_numpy(data).float(), sampling_rate
 
 
     def __getitem__(self, item):
+        audio_file = self.audio_files[item]
+        audio, sampling_rate = load_wav_to_torch(audio_file, self.sampling_rate)
+        return audio, self.gender_idx[item], self.labels[item], self.speaker_id[item]
 
-        pass
+    def __len__(self):
+        return len(self.audio_files)
 
 
     @staticmethod
-    def load():
-        train_annotations = pd.read_csv(local_vars.PREPROCESSED_AUDIO_MNIST_PATH + 'train_annoations.csv')
-        trainData = AudioDataset(train_annotations)
+    def load(sampling_rate=8000):
+        train_annotations = pd.read_csv(local_vars.PREPROCESSED_AUDIO_MNIST_PATH + 'train_annotations.csv')
+        trainData = AudioDataset(train_annotations, sampling_rate)
 
-        test_annotations = pd.read_csv(local_vars.PREPROCESSED_AUDIO_MNIST_PATH + 'test_annoations.csv')
-        testData = AudioDataset(test_annotations)
+        test_annotations = pd.read_csv(local_vars.PREPROCESSED_AUDIO_MNIST_PATH + 'test_annotations.csv')
+        testData = AudioDataset(test_annotations, sampling_rate)
 
         return trainData, testData
