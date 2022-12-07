ecc9eeb3357462c4e49ec55294aac5b347d4b43a
johbrob <johan.broberg92@gmail.com>
2022-12-07 14:21:27 +0100

    more accuracies

--- metrics_compiling.py
+++ metrics_compiling.py
@@ -2,6 +2,7 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 import torch
 
+
 def compute_metrics(secret, label, filter_gen_output, filter_disc_output, secret_gen_output,
                     secret_disc_output, losses):
     filter_gen_metrics = {'distortion_loss': losses['filter_gen']['distortion'].detach().cpu().numpy(),
@@ -22,30 +23,34 @@ def compute_metrics(secret, label, filter_gen_output, filter_disc_output, secret
     # print(secret_disc_output['fake_secret_score'].shape)
     fake_secret_preds_disc = torch.argmax(secret_disc_output['fake_secret_score'], 1).cpu().numpy()
     fake_secret_label_accuracy_disc = np.array(accuracy_score(secret_disc_output['fake_secret'].cpu().numpy(),
-                                                     fake_secret_preds_disc))
+                                                              fake_secret_preds_disc))
     real_secret_preds_disc = torch.argmax(secret_disc_output['real_secret_score'], 1).cpu().numpy()
     real_secret_label_accuracy_disc = np.array(accuracy_score(secret.cpu().numpy(), real_secret_preds_disc))
-    # generated_secret_accuracy_disc = accuracy_score(secret_disc_output['fake_secret'].cpu().numpy(),
-    #                                                 fake_secret_preds_disc)
+    generated_secret_accuracy_disc = np.array(accuracy_score(secret_gen_output['fake_secret'].cpu().numpy(),
+                                                             fake_secret_preds_disc))
+
     secret_disc_metrics = {'real_loss': losses['secret_disc']['real'].detach().cpu().numpy(),
                            'fake_loss': losses['secret_disc']['fake'].detach().cpu().numpy(),
                            'combined_loss': losses['secret_disc']['final'].detach().cpu().numpy(),
                            'fake_accuracy': fake_secret_label_accuracy_disc,
-                           'real_accuracy': real_secret_label_accuracy_disc}
+                           'real_accuracy': real_secret_label_accuracy_disc,
+                           'generated_accuracy': generated_secret_accuracy_disc}
 
     # label prediction
     label_preds_disc = torch.argmax(secret_disc_output['label_score'].data, 1).cpu().numpy()
     label_accuracy_disc = np.array(accuracy_score(label.cpu().numpy(), label_preds_disc))
     label_prediction_metrics = {'accuracy': label_accuracy_disc}
 
-    # print(filtered_secret_accuracy_disc, fake_secret_label_accuracy_disc, real_secret_label_accuracy_disc,
-    #       label_accuracy_disc)
-    # fixed_predicted = torch.argmax(fixed_pred_secret.data, 1)
-    # fixed_pred_secret_label = fixed_pred_secret_label.cpu().numpy()
-    # fixed_pred_secret_label_accuracy = accuracy_score(secret_label, fixed_pred_secret_label[:, 1])
+    secret_preds_disc = torch.argmax(secret_disc_output['secret_score'].data, 1).cpu().numpy()
+    real_secret_accuracy_disc = np.array(accuracy_score(secret.cpu().numpy(), secret_preds_disc))
+    fake_secret_accuracy_disc = np.array(
+        accuracy_score(secret_gen_output['fake_secret'].cpu().numpy(), secret_preds_disc))
+    secret_prediction_metrics = {'real_secret_accuracy': real_secret_accuracy_disc,
+                                 'fake_secret_accuracy': fake_secret_accuracy_disc}
 
-    return {'filter_gen': filter_gen_metrics, 'secret_gen': secret_gen_metrics, 'filter_disc': filter_disc_metrics,
-            'secret_disc': secret_disc_metrics, 'label_pred': label_prediction_metrics}
+    return {'filter_gen': filter_gen_metrics, 'secret_gen': secret_gen_metrics,
+            'filter_disc': filter_disc_metrics, 'secret_disc': secret_disc_metrics,
+            'label_pred': label_prediction_metrics, 'secret_pred': secret_prediction_metrics}
 
 
 def compile_metrics(metrics):
@@ -63,4 +68,3 @@ def aggregate_metrics(batch_metrics, metrics):
             raise NotImplementedError(f'Unexpected format of metric: {k}, {v}')
 
     return metrics
-
