fd78a2a93ab91f45b6ca4ae686b0ae264f761be3
johbrob <johan.broberg92@gmail.com>
2022-12-09 14:10:16 +0100

    fix

--- remote_training.py
+++ remote_training.py
@@ -23,14 +23,14 @@ def verify_args(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dataset", help="name of dataset")
+    parser.add_argument("-d", "--dataset", help="name of dataset", default='audiomnist')
     parser.add_argument("-s", "--settings", help="which pre-defined setting to use", default='debug')
     parser.add_argument("-g", "--gpu", help="specify which gpu to use. Enter 'no' to use cpu. "
                                             "Will also use cpu if no gpu is available", default='0')
     args = parser.parse_args()
     args = vars(args)
 
-    args = {'dataset': 'audiomnist', 'settings': 'debug', 'gpu': 0}
+    # args = {'dataset': 'audiomnist', 'settings': 'debug', 'gpu': 0}
     verify_args(args)
 
     if not torch.cuda.is_available() or args['gpu'] == 'no':
