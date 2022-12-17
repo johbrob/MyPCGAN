e7f3fcfd7ab3514249b30c69804147bfb28f7075
johbrob <johan.broberg92@gmail.com>
2022-12-17 13:15:33 +0100

    fix bug

--- utils.py
+++ utils.py
@@ -68,7 +68,7 @@ def freeze(model):
 
 def unfreeze(model):
     for param in model.parameters():
-        param.requires_grad = False
+        param.requires_grad = True
 
 # ----------- Saving Audio stuff -----------
 
